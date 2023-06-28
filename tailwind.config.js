04913f8b78fc6c9e3af6cec34db331569da2d7ab
minhthuong031103 <nominh67@gmail.com>
2023-06-28 23:37:21 +0700

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        c0: '#101010',
+        c1: '#131313',
+        c2: '#202329',
+        c3: '#8B8D93',
+        c4: '#6b8afd',
+        c5: '#2E343D',
+      },
+
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -15,4 +24,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
